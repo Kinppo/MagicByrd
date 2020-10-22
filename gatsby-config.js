297b6621dad68b8982dd3de09035897be2bf47c5
Mouaad Bk <boukiaou.mouaad@gmail.com>
2020-10-22 19:14:14 +0100

    last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,12 @@ module.exports = {
     author: `MBk`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-2V7XD5YZGT",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
